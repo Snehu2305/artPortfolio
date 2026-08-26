7860d2919c1ebf7a1041c0c39980497f346d597b
Snehal Dattatray Kumbhar <snehsujata2305@gmail.com>
2026-08-26 20:56:48 +0530

    Add files via upload

---
+++ Data/video.js
@@ -0,0 +1,221 @@
+const video = [
+    {
+        id:1,
+        title:"Happy Independence Day",
+        url: "https://www.youtube.com/embed/jFJYlWb5WvI",
+        category: "Modern"
+    },
+    {
+        id: 2,
+        title:"Ocean Sunset Mandala",
+        url: "https://www.youtube.com/embed/jFU9cenN8Yk",
+        category: "Modern"
+    },
+    {
+        id: 3,
+        title:"Bow Mandala",
+        url: "https://www.youtube.com/embed/Z4ot5-w8nrM",
+        category: "Modern"
+    },
+    {
+        id: 4,
+        title:"Blue-Green Mandala",
+        url: "https://www.youtube.com/embed/L27Lbx4nTe8",
+        category: "Mandala"
+    },
+    {
+        id: 5,
+        title:"Ashadhi Ekadashi",
+        url: "https://www.youtube.com/embed/_MT-qSH1rbo",
+        category: "Festival"
+    },
+    {
+        id: 6,
+        title:"Vitthal",
+        url: "https://www.youtube.com/embed/r-MIsEiagJ8",
+        category: "Spiritual"
+    },
+    {
+        id: 7,
+        title:"Blue-Maroon Mandala",
+        url: "https://www.youtube.com/embed/dcg42XSWyzU",
+        category: "Mandala"
+    },
+    {
+        id: 8,
+        title:"Purple Mandala",
+        url: "https://www.youtube.com/embed/Dfma3sYtydc",
+        category: "Mandala"
+    },
+    {
+        id: 9,
+        title: "Cute Ghost Mandala",
+        url: "https://www.youtube.com/embed/pF6B3wzzV3I",
+        category: "Modern",
+    },
+    {
+        id: 10,
+        title: "Reverse Pattern Mandala",
+        url: "https://www.youtube.com/embed/dCgcIOZ-yOw",
+        category: "Mandala",
+    },
+    {
+        id: 11,
+        title: "Color Wheel Mandala",
+        url: "https://www.youtube.com/embed/FT2tgXpkC9E",
+        category: "Mandala",
+    },
+    {
+        id: 12,
+        title: "International Yoga Day",
+        url: "https://www.youtube.com/embed/a2Q24u2CYsE",
+        category: "Modern",
+    },
+    {
+        id: 13,
+        title: "Cute Elephant Mandala",
+        url: "https://www.youtube.com/embed/hLZHbQ0ncms",
+        category: "Modern",
+    },
+    {
+        id: 14,
+        title: "Dual Tone Abstract Mandala",
+        url: "https://www.youtube.com/embed/zXw86ZV-vD8",
+        category: "Modern",
+    },
+    {
+        id: 15,
+        title: "Friendship Mandala",
+        url: "https://www.youtube.com/embed/iv54iOkIIvY",
+        category: "Modern",
+    },
+    {
+        id: 16,
+        title: "Butterfly Mandala",
+        url: "https://www.youtube.com/embed/OgryU8ABPtw",
+        category: "Modern",
+    },
+    {
+        id: 17,
+        title: "Pink Harmony Mandala",
+        url: "https://www.youtube.com/embed/cjg9E8p5NM0",
+        category: "Mandala",
+    },
+    {
+        id: 18,
+        title: "Avacado Mandala",
+        url: "https://www.youtube.com/embed/_lTBaI1NBTw",
+        category: "Modern",
+    },
+    {
+        id: 19,
+        title: "Smiley Mandala",
+        url: "https://www.youtube.com/embed/mlQk4QR9z84",
+        category: "Modern",
+    },
+    {
+        id: 20,
+        title: "Ranibow Mandala",
+        url: "https://www.youtube.com/embed/6ToHbw8YvdQ",
+        category: "Modern",
+    },
+    {
+        id: 21,
+        title: "Half Mandala",
+        url: "https://www.youtube.com/embed/6SW4-rxgE1Y",
+        category: "Mandala",
+    },
+    {
+        id: 22,
+        title: "Triangle Mandala",
+        url: "https://www.youtube.com/embed/esW8a1lYE0w",
+        category: "Modern",
+    },
+    {
+        id: 23,
+        title: "Curves Only Mandala",
+        url: "https://www.youtube.com/embed/nDjqLuROyro",
+        category: "Mandala",
+    },
+    {
+        id: 24,
+        title: "Double Layer Mandala",
+        url: "https://www.youtube.com/embed/bQHZX35ybJQ",
+        category: "Mandala",
+    },
+    {
+        id: 25,
+        title: "Pattern drawing",
+        url: "https://www.youtube.com/embed/MEqJ5EUbqL8",
+        category: "Mandala",
+    },
+    {
+        id: 26,
+        title: "Zendot Mandala",
+        url: "https://www.youtube.com/embed/LLUGeyvAsX0",
+        category: "Mandala",
+    },
+    {
+        id: 27,
+        title: "Mahashivratri Mandala",
+        url: "https://www.youtube.com/embed/CzkuzLo50K8",
+        category: "Festival",
+    },
+    {
+        id: 28,
+        title: "Pattern design",
+        url: "https://www.youtube.com/embed/0ze53ThNfCc",
+        category: "Mandala",
+    },
+    {
+        id: 29,
+        title: "3 color Mandala",
+        url: "https://www.youtube.com/embed/vgQaP95SBf0",
+        category: "Mandala",
+    },
+    {
+        id: 30,
+        title: "Lotus Mandala",
+        url: "https://www.youtube.com/embed/aQ1IKTmvXZ4",
+        category: "Modern",
+    },
+    {
+        id: 31,
+        title: "2 color Mandala",
+        url: "https://www.youtube.com/embed/ysH5cnGbARc",
+        category: "Mandala",
+    },
+    {
+        id: 32,
+        title: "Green Mandala",
+        url: "https://www.youtube.com/embed/0nHSgRKgWow",
+        category: "Mandala",
+    },
+    {
+        id: 33,
+        title: "Star Mandala",
+        url: "https://www.youtube.com/embed/aJD8vOUsgnM",
+        category: "Modern",
+    },
+    {
+        id: 34,
+        title: "Geometric Mandala",
+        url: "https://www.youtube.com/embed/m5aUb9kyQlc",
+        category: "Mandala",
+    },
+    {
+        id: 35,
+        title: "Full Page Mandala",
+        url: "https://www.youtube.com/embed/N76m_CFbyAk",
+        category: "Modern",
+    },
+    {
+        id: 36,
+        title: "Bold Symmetry Mandala",
+        url: "https://www.youtube.com/embed/f896v9U94W4",
+        category: "Mandala",
+    },
+    
+]
+
+export default video
